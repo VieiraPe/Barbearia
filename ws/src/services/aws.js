40b4864a537cc7c451fafcba3c77bb4de9a72bd6
Pedro Vieira <ppgvsilva@gmail.com>
2025-01-17 21:47:40 -0300

    Atualização do AWS2 para o AWS3

--- ws/src/services/aws.js
+++ ws/src/services/aws.js
@@ -1,70 +1,61 @@
-import AWS from "aws-sdk";
+import { S3Client, PutObjectCommand, DeleteObjectCommand } from "@aws-sdk/client-s3";
 import "dotenv/config";
+
+const s3Client = new S3Client({
+  region: process.env.AWS_REGION,
+  credentials: {
+    accessKeyId: process.env.IAM_USER_KEY,
+    secretAccessKey: process.env.IAM_USER_SECRET,
+  },
+});
 
 export default {
   IAM_USER_KEY: process.env.IAM_USER_KEY,
   IAM_USER_SECRET: process.env.IAM_USER_SECRET,
   BUCKET_NAME: process.env.BUCKET_NAME,
   AWS_REGION: process.env.AWS_REGION,
+
+  // Função para upload de arquivos no S3
   uploadToS3: function (file, filename, acl = "public-read") {
-    return new Promise((resolve, reject) => {
-      let IAM_USER_KEY = this.IAM_USER_KEY;
-      let IAM_USER_SECRET = this.IAM_USER_SECRET;
-      let BUCKET_NAME = this.BUCKET_NAME;
+    return new Promise(async (resolve, reject) => {
+      const params = {
+        Bucket: this.BUCKET_NAME,
+        Key: filename,
+        Body: file.data,
+        ACL: acl,
+      };
 
-      let s3bucket = new AWS.S3({
-        accessKeyId: IAM_USER_KEY,
-        secretAccessKey: IAM_USER_SECRET,
-        Bucket: BUCKET_NAME,
-      });
+      const command = new PutObjectCommand(params);
 
-      s3bucket.createBucket(function () {
-        var params = {
-          Bucket: BUCKET_NAME,
-          Key: filename,
-          Body: file.data,
-          ACL: acl,
-        };
-
-        s3bucket.upload(params, function (err, data) {
-          if (err) {
-            console.log(err);
-            return resolve({ error: true, message: err.message });
-          }
-          console.log(data);
-          return resolve({ error: false, message: data });
-        });
-      });
+      try {
+        const data = await s3Client.send(command);
+        console.log("Upload Success: ", data);
+        resolve({ error: false, message: data });
+      } catch (err) {
+        console.log("Upload Error: ", err);
+        reject({ error: true, message: err.message });
+      }
     });
   },
+
+  // Função para deletar arquivos do S3
   deleteFileS3: function (key) {
-    return new Promise((resolve, reject) => {
-      let IAM_USER_KEY = this.IAM_USER_KEY;
-      let IAM_USER_SECRET = this.IAM_USER_SECRET;
-      let BUCKET_NAME = this.BUCKET_NAME;
+    return new Promise(async (resolve, reject) => {
+      const params = {
+        Bucket: this.BUCKET_NAME,
+        Key: key,
+      };
 
-      let s3bucket = new AWS.S3({
-        accessKeyId: IAM_USER_KEY,
-        secretAccessKey: IAM_USER_SECRET,
-        Bucket: BUCKET_NAME,
-      });
+      const command = new DeleteObjectCommand(params);
 
-      s3bucket.createBucket(function () {
-        s3bucket.deleteObject(
-          {
-            Bucket: BUCKET_NAME,
-            Key: key,
-          },
-          function (err, data) {
-            if (err) {
-              console.log(err);
-              return resolve({ error: true, message: err });
-            }
-            console.log(data);
-            return resolve({ error: false, message: data });
-          }
-        );
-      });
+      try {
+        const data = await s3Client.send(command);
+        console.log("Delete Success: ", data);
+        resolve({ error: false, message: data });
+      } catch (err) {
+        console.log("Delete Error: ", err);
+        reject({ error: true, message: err.message });
+      }
     });
   },
 };
